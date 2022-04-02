b043837f927fd0aa709e755aca98c24eefdb9f04
xiangshilin <295755973@qq.com>
2022-04-03 00:20:36 +0800

    ✨ feat: 更新部署SSL

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as serveStatic from 'serve-static';
 import { join } from 'path';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const keyFile  = readFileSync('/www/server/panel/vhost/cert/api.resonance.fun/privkey.pem');
+  const certFile = readFileSync('/www/server/panel/vhost/cert/api.resonance.fun/fullchain.pem');
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    }
+  });
   app.use('/public', serveStatic(join(__dirname, '../public'), {
     maxAge: '1d',
     extensions: ['jpg', 'jpeg', 'png', 'gif'],
   }));
-  //....
   await app.startAllMicroservices();
   await app.listen(999);
 }
