065999b0bda699070c798c60b7b819139c3e7bab
木木 <295755973@qq.com>
2022-05-27 02:34:14 +0000

    🐞 fix: ssl

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as serveStatic from 'serve-static';
 import { join } from 'path';
-import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const keyFile  = readFileSync('/www/server/panel/vhost/cert/api.resonance.fun/privkey.pem');
-  const certFile = readFileSync('/www/server/panel/vhost/cert/api.resonance.fun/fullchain.pem');
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: keyFile,
-      cert: certFile,
-    }
-  });
+  const app = await NestFactory.create(AppModule, {});
   app.use('/public', serveStatic(join(__dirname, '../public'), {
     maxAge: '1d',
     extensions: ['jpg', 'jpeg', 'png', 'gif'],
